977c89d1bb617b70b5d595896eb71a446a653ded
Ocheben <ocheben@gmail.com>
2019-04-24 13:00:08 +0100

    Added a path

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 
 const port = process.env.PORT || 7000;
 
+app.get('/', (req, res) => {
+    res.send('Hello, Hire a Service')
+})
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
 })
