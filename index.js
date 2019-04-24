aa3b7b9571ffe18fc265c81600b37f5153475943
Ocheben <ocheben@gmail.com>
2019-04-24 13:07:06 +0100

    Changed Port

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 7000;
 app.get('/', (req, res) => {
     res.send('Hello, Hire a Service')
 })
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`App running on port ${port}`)
 })
