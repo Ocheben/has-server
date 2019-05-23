730c525222578460550b7afd5150590c499422cd
Ocheben <ocheben@gmail.com>
2019-05-23 16:07:42 +0100

    cors8

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const db = require('./queries');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 7000;
+
+app.use(express.json())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }))
-app.use(express.json())
-app.use(cors());
-const port = process.env.PORT || 7000;
+
 
 app.get('/', (req, res) => {
     res.send('Hello, Hire a Service')
