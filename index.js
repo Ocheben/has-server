8957155cfd9db32be95bf32e054e5476250a6dd9
Ocheben <ocheben@gmail.com>
2019-05-23 13:18:39 +0100

    Cors2

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ app.use(express.json())
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
-  });
+});
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
