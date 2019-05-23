5cabd9aaf7dd8093dc21225c3e8f92bdb21f5413
Ocheben <ocheben@gmail.com>
2019-05-23 13:37:42 +0100

    cors 6

--- index.js
+++ index.js
@@ -5,22 +5,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express()
-// app.options('/', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", '*');
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//     next();
-// });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }))
 app.use(express.json())
 app.use(cors());
-
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 7000;
 
 app.get('/', (req, res) => {
     res.send('Hello, Hire a Service')
